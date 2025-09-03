4100b4f65a572ba82bc11da6c9f6c2b1001650ca
AswiniKumarDeka <145135333+AswiniKumarDeka@users.noreply.github.com>
2025-09-04 00:34:36 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $host     = "d2s3c1u3jp1c738qktg0-a.oregon-postgres.render.com";
 $port     = "5432";
 $dbname   = "aparajita_db";
 $user     = "aparajita_db_user";
-$password = "your-long-password";
+$password = "yiDKShPoESiyS151fldPy1QJIZVZ8LG1";
 
 try {
     $pdo = new PDO(
